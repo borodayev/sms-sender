5c5f90a9b89106448b813a38a6ff08e086ac6367
frankast <borodaev97@gmail.com>
2018-03-03 16:14:21 +0600

    test: wip

--- src/__tests__/Smsc-test.js
+++ src/__tests__/Smsc-test.js
@@ -1,6 +1,6 @@
 // @flow
 
-// import fetchMock from 'fetch-mock';
+import fetchMock from 'fetch-mock';
 import Smsc from '../Smsc';
 
 describe('Smsc', () => {
@@ -8,20 +8,47 @@ describe('Smsc', () => {
   const password = 'test_dev_kz';
   const smsc = new Smsc({ login, password });
 
-  it('send', async () => {
-    const phones = ['+77081949582'];
+  beforeEach(() => {
+    fetchMock.reset();
+  });
+
+  it('sendSms', async () => {
+    const phones = '+77718637484';
     const message = 'hello';
 
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/send.php?login=${login}&psw=${password}&phones=${phones}&mes=${message}&fmt=3`,
+      response: { cnt: 1, id: 1 },
+    });
+
+    const res = await smsc.sendSms(phones, message);
+    expect(res).toEqual({ cnt: 1, id: 1 });
+  });
+
+  it('getStatus', async () => {
+    const phone = '+77718637484';
+    const id = 1;
+
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/status.php?login=${login}&psw=${password}&phone=${phone}&id=${id}&fmt=3`,
+      response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
+    });
+
+    const res = await smsc.getStatus(id, phone);
+    expect(res).toEqual({
+      last_date: '03.03.2018 15:47:14',
+      last_timestamp: 1520070434,
+      status: 1,
+    });
+  });
+
+  it.skip('getBalance', async () => {
     // fetchMock.mock({
-    //   matcher: `http://smsc.ru/sys/send.php?login=${login}&psw=${password}&phones=${phones}&mes=${message}`,
-    //   response: () => {
-    //     return 'ok';
-    //   },
+    //   matcher: `http://smsc.ru/sys/balance.php?login=${login}&psw=${password}&fmt=3`,
+    //   response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
     // });
 
-    // smsc.send = jest.fn();
-    const res = await smsc.send({ phones, message });
-    expect(res).toBe();
-    // expect(smsc.send).toBeCalledWith({ message: 'hello', phones: '+77081949582' });
+    const res = await smsc.getBalance();
+    expect(res).toEqual();
   });
 });
