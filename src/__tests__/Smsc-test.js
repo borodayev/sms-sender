98fc65de6a1d5b8e183c0ff8170de47f380a0711
frankast <borodaev97@gmail.com>
2018-03-04 18:09:24 +0600

    test: fix tests

--- src/__tests__/Smsc-test.js
+++ src/__tests__/Smsc-test.js
@@ -3,6 +3,8 @@
 import fetchMock from 'fetch-mock';
 import Smsc from '../Smsc';
 
+jest.mock('../schema/db');
+
 describe('Smsc', () => {
   const login = 'test_dev_kz';
   const password = 'test_dev_kz';
@@ -17,12 +19,24 @@ describe('Smsc', () => {
     const message = 'hello';
 
     fetchMock.mock({
-      matcher: `http://smsc.ru/sys/send.php?login=${login}&psw=${password}&phones=${phones}&mes=${message}&fmt=3`,
-      response: { cnt: 1, id: 1 },
+      matcher: `http://smsc.ru/sys/send.php?login=${login}&psw=${password}&phones=${phones}&mes=${message}&fmt=3&cost=3`,
+      response: { balance: '84.75', cnt: 1, cost: '0', id: 24 },
     });
 
     const res = await smsc.sendSms(phones, message);
-    expect(res).toEqual({ cnt: 1, id: 1 });
+    expect(res).toEqual({ balance: '84.75', cnt: 1, cost: '0', id: 24 });
+  });
+
+  it('getCost', async () => {
+    const phones = '+77718637484';
+    const message = 'hello';
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/send.php?login=${login}&psw=${password}&phones=${phones}&mes=${message}&fmt=3&cost=1`,
+      response: { cnt: 1, cost: '0' },
+    });
+
+    const res = await smsc.getCost(phones, message);
+    expect(res).toEqual({ cnt: 1, cost: '0' });
   });
 
   it('getStatus', async () => {
@@ -42,13 +56,13 @@ describe('Smsc', () => {
     });
   });
 
-  it.skip('getBalance', async () => {
-    // fetchMock.mock({
-    //   matcher: `http://smsc.ru/sys/balance.php?login=${login}&psw=${password}&fmt=3`,
-    //   response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
-    // });
+  it('getBalance', async () => {
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/balance.php?login=${login}&psw=${password}&fmt=3`,
+      response: { balance: '84.75' },
+    });
 
     const res = await smsc.getBalance();
-    expect(res).toEqual();
+    expect(res).toEqual({ balance: '84.75' });
   });
 });
