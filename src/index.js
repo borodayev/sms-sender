4b85b318c1e78c91a6516f9e0dbe89d7ab386a10
frankast <borodaev97@gmail.com>
2018-03-04 18:09:42 +0600

    refactor: create utils.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // @flow
-// import Smsc from './Smsc';
-//
-// const smsc = new Smsc({ login: 'lol', pass: '123' });
-//
-// smsc.getCredentials();
+
+import Smsc from './Smsc';
+import DB from './schema/db';
+
+DB.init();
+
+const login = 'test_dev_kz';
+const password = 'test_dev_kz';
+const smsc = new Smsc({ login, password });
+
+smsc.sendSms('+77718637484', 'new message');
+
+export default Smsc;
