21d910a91e8813a872ec30a1cdec647628385755
Borodayev Valeriy <borodaev97@gmail.com>
2018-03-06 14:25:34 +0600

    wip

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const login = 'test_dev_kz';
 const password = 'test_dev_kz';
 const smsc = new Smsc({ login, password });
 
-smsc.sendSms('+77718637484', 'new message');
+smsc.sendSms('+77718637484', 'new message').then(res => console.log(res)); // eslint-disable-line no-console
+smsc.getStatus(30, '+77718637484').then(res => console.log(res)); // eslint-disable-line no-console
+smsc.getCost('+77718637484', 'message').then(res => console.log(res)); // eslint-disable-line no-console
+smsc.getBalance().then(res => console.log(res)); // eslint-disable-line no-console
 
 export default Smsc;
