70b09548c294148aaae30a5a763bd0b84836c43d
frankast <borodaev97@gmail.com>
2018-03-13 00:11:58 +0600

    test: refactor tests for getBalance(), getStatus()

--- src/transporters/__tests__/Smsc-test.js
+++ src/transporters/__tests__/Smsc-test.js
@@ -14,7 +14,7 @@ describe('Smsc', () => {
 
   it('sendSms', async () => {
     fetchMock.mock({
-      matcher: `http://smsc.ru/sys/send.php?phones=77718637484&mes=hello&fmt=3&cost=3&login=test_dev_kz&psw=test_dev_kz`,
+      matcher: `http://smsc.ru/sys/send.php?login=test_dev_kz&psw=test_dev_kz&phones=77718637484&mes=hello&fmt=3&cost=3`,
       response: { balance: '84.75', cnt: 1, cost: '0', id: 24 },
     });
 
@@ -28,38 +28,35 @@ describe('Smsc', () => {
 
   it('getCost', async () => {
     fetchMock.mock({
-      matcher: `http://smsc.ru/sys/send.php?phones=77718637484&mes=hello&fmt=3&cost=1&login=test_dev_kz&psw=test_dev_kz`,
+      matcher: `http://smsc.ru/sys/send.php?login=test_dev_kz&psw=test_dev_kz&cost=1&phones=77718637484&mes=hello&fmt=3`,
       response: { cnt: 1, cost: '0' },
     });
 
     const res = await smsc.getCost({ phones: '77718637484', mes: 'hello', fmt: 3 });
     expect(res).toEqual({ cost: '0', response: { cnt: 1, cost: '0' } });
   });
-  //
-  // it('getStatus', async () => {
-  //   const phone = '+77718637484';
-  //   const id = 1;
-  //
-  //   fetchMock.mock({
-  //     matcher: `http://smsc.ru/sys/status.php?login=${login}&psw=${password}&phone=${phone}&id=${id}&fmt=3`,
-  //     response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
-  //   });
-  //
-  //   const res = await smsc.getStatus(id, phone);
-  //   expect(res).toEqual({
-  //     last_date: '03.03.2018 15:47:14',
-  //     last_timestamp: 1520070434,
-  //     status: 1,
-  //   });
-  // });
-  //
-  // it('getBalance', async () => {
-  //   fetchMock.mock({
-  //     matcher: `http://smsc.ru/sys/balance.php?login=${login}&psw=${password}&fmt=3`,
-  //     response: { balance: '84.75' },
-  //   });
-  //
-  //   const res = await smsc.getBalance();
-  //   expect(res).toEqual({ balance: '84.75' });
-  // });
+
+  it('getStatus', async () => {
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/status.php?login=test_dev_kz&psw=test_dev_kz&id=37&phone=77718637484&fmt=3`,
+      response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
+    });
+
+    const messageId = `37-77718637484`;
+    const res = await smsc.getStatus(messageId);
+    expect(res).toEqual({
+      response: { last_date: '03.03.2018 15:47:14', last_timestamp: 1520070434, status: 1 },
+      status: 1,
+    });
+  });
+
+  it('getBalance', async () => {
+    fetchMock.mock({
+      matcher: `http://smsc.ru/sys/balance.php?login=test_dev_kz&psw=test_dev_kz&fmt=3`,
+      response: { balance: '84.75' },
+    });
+
+    const res = await smsc.getBalance({ fmt: 3 });
+    expect(res).toEqual({ balance: '84.75' });
+  });
 });
