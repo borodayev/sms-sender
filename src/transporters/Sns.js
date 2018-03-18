3d45fb558a052f2228e8cbf2b8944be2b0db5c99
frankast <borodaev97@gmail.com>
2018-03-18 23:20:23 +0600

    wip: AWS.SNS is on processing

---
+++ src/transporters/Sns.js
@@ -0,0 +1,45 @@
+// @flow
+
+import AWS from 'aws-sdk';
+
+import type {
+  SendSmsResponseT,
+  // GetCostResponseT,
+  // GetStatusResponseT,
+  // GetBalanceResponseT,
+  // SmsStatusT,
+  // ProviderI,
+} from '../definitions';
+
+type SnsParamsT = {
+  accessKeyId?: string,
+  secretAccessKey?: string,
+  region: string,
+};
+
+export default class Sns /* implements ProviderI */ {
+  sns: Object;
+
+  constructor(params: SnsParamsT) {
+    const { accessKeyId, secretAccessKey, region } = params || {};
+    if (accessKeyId && secretAccessKey) {
+      this.sns = new AWS.SNS({ accessKeyId, secretAccessKey, region });
+    } else {
+      this.sns = new AWS.SNS({ region });
+    }
+  }
+
+  async sendSms(phone: string, message: string): Promise<SendSmsResponseT> {
+    const params = {
+      Message: message,
+      MessageStructure: 'string',
+      PhoneNumber: phone,
+    };
+    const rawResponse = await this.sns.publish(params);
+    return rawResponse;
+  }
+  // async getCost(phone: string, message: string): Promise<GetCostResponseT> {}
+  // async getStatus(messageId: string): Promise<GetStatusResponseT> {}
+  // async getBalance(): Promise<GetBalanceResponseT> {}
+  // getProviderName(): string {}
+}
