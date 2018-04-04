f516d21518a867e3bf6ce853d86d6048746291cc
frankast <borodaev97@gmail.com>
2018-04-04 18:50:21 +0600

    test(Sns): move mock of 'Sns.sendSms()' to test

--- src/transporters/__tests__/Sns-test.js
+++ src/transporters/__tests__/Sns-test.js
@@ -2,11 +2,18 @@
 
 import Sns from '../Sns';
 
-jest.mock('../Sns');
+const sns = new Sns({ region: 'eu-west-1' });
+sns.sendSms = jest.fn().mockImplementation(() => {
+  return {
+    messageId: '8e1e071f-a27d-55db-8ba9-542c4f9c8a73',
+    rawResponse: {
+      MessageId: '8e1e071f-a27d-55db-8ba9-542c4f9c8a73',
+      ResponseMetadata: { RequestId: '919bdf65-2969-5a03-ae88-8c56c48825ab' },
+    },
+  };
+});
 
 describe('Sns', () => {
-  const sns = new Sns({ region: 'eu-west-1' });
-
   // send message
   it('sendSms', async () => {
     const res = await sns.sendSms('+77081949582', 'Hi');
