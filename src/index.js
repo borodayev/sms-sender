999efc2864ac994695d0615f206643896b9a74a8
ArtiStyle <37900505+ArtiStyle@users.noreply.github.com>
2018-10-23 11:58:45 +0600

    add: Aero sms service, PlayMobile sms service (#25)

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@
 export { default as Smsc } from './transporters/Smsc';
 export { default as Sns } from './transporters/Sns';
 export { default as Mobizon } from './transporters/Mobizon';
+export { default as PlayMobile } from './transporters/PlayMobile';
+export { default as Aero } from './transporters/Aero';
